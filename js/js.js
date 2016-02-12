dd6d4f0bc4e6d4432010ec77c8aaf472259df8cc
dezerb <dezerb@gmail.com>
2016-02-12 16:24:58 +0100

    minesweeper final commit

--- js/js.js
+++ js/js.js
@@ -1,17 +1,29 @@
 $(document).ready(function(){
+  window.minesweeper.setlvl("init");
   window.minesweeper.addGrid();
   window.minesweeper.render();
   window.minesweeper.addMines();
+  window.minesweeper.timer();
 });
 
 window.minesweeper = window.minesweeper || {};
 
+window.minesweeper.timer = function(){
+  time  ++;
+  document.getElementById('time').innerHTML = '<p>'+time+'</p>';
+   timer =  window.setTimeout(window.minesweeper.timer, 1000);
+}; 
+
 window.minesweeper.render = function(){
   $('section').append('<div id=menubar></div>')
   $('#menubar').append('<button id=displaymenu onclick=window.minesweeper.displaymenu()>Game</button>');
-  $('section').append('<ul id=menu><li>Reset</li><li>Settings</li></ul>'); 
-  $('section').append('<div id=mainbar><div id=flagleft class=infobox></div><div id=smile></div><div id=time class=infobox></div>'); 
+  $('section').append('<ul id=menu><li onclick=window.minesweeper.reset()>Reset</li><li onclick=window.minesweeper.settings()  >Settings</li></ul>'); 
+  $('section').append('<div id=mainbar><div id=flagleft class=infobox></div><div id=smile onclick=window.minesweeper.reset()></div><div id=time class=infobox></div>'); 
   $('section').append('<div id=board></div>'); 
+  $('section').append('<div id=settings><button onclick=window.minesweeper.setlvl("easy")>EASY</button><button onclick=window.minesweeper.setlvl("medium")>MEDIUM</button><button onclick=window.minesweeper.setlvl("hard")>HARD</button></div>');
+  document.getElementById('smile').innerHTML = '<p>&#x263a;</p>';
+  $('section').append('<div id=fail><p>GAME OVER</p></div>');
+
   document.getElementById('flagleft').innerHTML = '<p>'+flagLeft+'</p>';
 
 
@@ -35,14 +47,68 @@ window.minesweeper.displaymenu = function(){
   $('#menu').toggle()
 };
 
+window.minesweeper.reset = function(){
+  $('section').empty();
+  flagLeft = gridHeight;
+  clearTimeout(timer);
+  window.minesweeper.addGrid();
+  window.minesweeper.render();
+  window.minesweeper.addMines();
+  window.minesweeper.timer();
+  $('.cell').css("width", ""+100/gridHeight+"%");
+  $('.row').css("height", ""+100/gridHeight+"%");
+  $('.row').css("font-size", ""+100/gridHeight/2+"vh");
+
+};
+
+window.minesweeper.settings = function() {
+  $('#settings').toggle();
+  $('#menu').toggle()
+};
+
+window.minesweeper.setlvl = function(level) {
+  if ( level == "init") {
+    gridHeight = 10;
+    gridWidth = 10; 
+    minesNumber = 10;
+    flagLeft = 10; 
+    $('.cell').css("width", "10%");
+    $('.row').css("height", "10%");
+  } else if ( level == "easy" ) {
+    gridHeight = 10;
+    gridWidth = 10; 
+    minesNumber = 10;
+    flagLeft = 10; 
+    reset();
+  } else if ( level == "medium" ) {
+    gridHeight = 15;
+    gridWidth = 15; 
+    minesNumber = 25;
+    flagLeft = 15; 
+    $('.cell').css("width", "5%");
+    $('.row').css("height", "5%");
+    reset();
+  } else if ( level == "hard" ) {
+    gridHeight = 30;
+    gridWidth = 30; 
+    minesNumber = 50;
+    flagLeft = 50; 
+    $('.cell').css("width", "2%");
+    $('.row').css("height", "2%");
+    reset();
+  };
+  
+  reset = function() {
+    $('#settings').toggle();
+    $('#menu').toggle()
+    window.minesweeper.reset();
+  };
+};
 
 window.minesweeper.addGrid = function() {
-  gridHeight = 10;
-  gridWidth = 10; 
-  minesNumber = 10;
   grid = [];
-  flagLeft = 10;
   gridMines = [];
+  time = 0;
 
   for ( var i = 0; i < gridHeight; i++) {
     grid.push([]);
@@ -56,19 +122,19 @@ window.minesweeper.addGrid = function() {
 };
 
 window.minesweeper.render.addRow = (function(element, index, array) {
-  $('#board').append("<div class=row id="+index+"></div>");
+  $('#board').append("<div class=row id=row-"+index+"></div>");
 });
 
 window.minesweeper.render.addCell = (function(i,j,val) {
-  $('#'+i+'').append("<div id="+i+j+" class='cell cell-no-"+j+"' onclick=window.minesweeper.check("+i+","+j+")>"+val+"</div>"); 
+  $('#row-'+i+'').append("<div id="+i+'x'+j+" class='cell cell-no-"+j+"' onclick=window.minesweeper.check("+i+","+j+")>"+val+"</div>"); 
 });
 
 window.minesweeper.addMines = (function() {
 arrayMines = [];
 
   for ( var i = 0; arrayMines.length < minesNumber; i++) {
-     first = Math.floor((Math.random() * 9) );  
-     second = Math.floor((Math.random() * 9) );  
+     first = Math.floor((Math.random() * gridHeight) );  
+     second = Math.floor((Math.random() * gridHeight) );  
     if ( arrayMines.join('x').includes(''+first+','+second+'') ) {
       i--;
     }
@@ -86,23 +152,26 @@ arrayMines = [];
 
 window.minesweeper.check = (function(i,j) {
   if ( arrayMines.join('x').includes(''+i+','+j+'') ) {
-   document.getElementById(''+i+''+j+'').innerHTML = "<p>&#128163;</p>";
-   document.getElementById(''+i+''+j+'').style.background = '#E4E4E4'
-   document.getElementById(''+i+''+j+'').className += " checked" 
+    document.getElementById(''+i+'x'+j+'').innerHTML = "<p>&#128163;</p>";
+    document.getElementById(''+i+'x'+j+'').style.background = '#E4E4E4';
+    document.getElementById(''+i+'x'+j+'').className += " checked";
+    window.minesweeper.gameover("fail");
   }
   else if ( gridMines[i][j] == 0)  {
     arrayCheck = [];
     arrayCheck.push(i,j); 
     window.minesweeper.checkEMPTY(arrayCheck, arrayCheck);
-    document.getElementById(''+i+''+j+'').style.background = '#E4E4E4'
-    document.getElementById(''+i+''+j+'').className += " checked" 
+    document.getElementById(''+i+'x'+j+'').style.background = '#E4E4E4'
+    document.getElementById(''+i+'x'+j+'').className += " checked" 
   }
   else {
-    document.getElementById(''+i+''+j+'').style.background = '#E4E4E4'
-    document.getElementById(''+i+''+j+'').innerHTML = "<p>"+gridMines[i][j]+"</p>";
-    document.getElementById(''+i+''+j+'').className += " checked" 
+    document.getElementById(''+i+'x'+j+'').style.background = '#E4E4E4'
+    document.getElementById(''+i+'x'+j+'').innerHTML = "<p>"+gridMines[i][j]+"</p>";
+    document.getElementById(''+i+'x'+j+'').className += " checked" 
   };
   
+   window.minesweeper.checkWin();  
+
 });
 
 window.minesweeper.createMinesGrid = (function() {
@@ -113,6 +182,7 @@ window.minesweeper.createMinesGrid = (function() {
       window.minesweeper.checkDOWN(i,j);
       window.minesweeper.checkLEFT(i,j);
       window.minesweeper.checkRIGHT(i,j);
+      window.minesweeper.checkEDGES(i,j);
       if ( gridMines[i][j] == "m" ) {
       } else {
         gridMines[i][j] = minesTouched;
@@ -132,7 +202,7 @@ window.minesweeper.checkUP = (function(i,j) {
 });
 
 window.minesweeper.checkDOWN = (function(i,j) {
-  if ( i == 9 ) {
+  if ( i == gridHeight - 1) {
     return;
   } else {
     if ( gridMines[i+1][j] == "m"    ) {
@@ -152,13 +222,43 @@ window.minesweeper.checkLEFT = (function(i,j) {
 });
 
 window.minesweeper.checkRIGHT = (function(i,j) {
-  if ( j == 9 ) {
+  if ( j == gridHeight -1 ) {
     return;
   } else {
     if ( gridMines[i][j+1] == "m"    ) {
       minesTouched ++;
     };
   }
+});
+
+window.minesweeper.checkEDGES = (function(i,j) {
+  if ( i == 0 || j == 0 ) { //up-left
+  } else {
+    if ( gridMines[i-1][j-1] == "m"    ) {
+      minesTouched ++;
+    };
+  };
+
+  if ( i == 0 || j == gridHeight-1 ) { //up-right
+  } else {
+    if ( gridMines[i-1][j+1] == "m"    ) {
+      minesTouched ++;
+    };
+  };
+
+  if ( i == gridHeight-1 || j == gridHeight-1 ) { //down-right
+  } else {
+    if ( gridMines[i+1][j+1] == "m"    ) {
+      minesTouched ++;
+    };
+  };
+
+  if ( i == gridHeight-1 || j == 0 ) { //down-left
+  } else {
+    if ( gridMines[i+1][j-1] == "m"    ) {
+      minesTouched ++;
+    };
+  };
 });
 
 $(document).on("contextmenu", ".cell", (function(event) {
@@ -179,20 +279,91 @@ $(document).on("contextmenu", ".cell", (function(event) {
 
 window.minesweeper.checkEMPTY = (function(arr, tempArr) {
   tempArray = tempArr.slice(0); 
+  tempArray2 = tempArr.slice(0); 
+  tempArray.push("x");
   globalArray = arr.slice(0);
-  window.minesweeper.checkemptyup(globalArray); 
-  window.minesweeper.checkemptydown(globalArray);
-  window.minesweeper.checkemptyleft(globalArray); 
-  window.minesweeper.checkemptyright(globalArray); 
+
+  localArr = arr.slice(0);
+  window.minesweeper.checkEMPTY2(localArr);  
+
+  for ( var i = 0; i < tempArray2.length; i = i+2 ) {
+    document.getElementById(''+tempArray2[i]+'x'+tempArray2[i+1]+'').style.background = '#E4E4E4'
+    document.getElementById(''+tempArray2[i]+'x'+tempArray2[i+1]+'').className += " checked" 
+    document.getElementById(''+tempArray2[i]+'x'+tempArray2[i+1]+'').innerHTML = "<p>"+gridMines[(tempArray2[i])][tempArray2[i+1]]+"</p>";
+    if ( $('#'+tempArray2[i]+'x'+tempArray2[i+1]+'').attr('class').includes('flag') ) {
+      flagLeft ++;
+      document.getElementById('flagleft').innerHTML = '<p>'+flagLeft+'</p>'
+    }
+  };
 });
 
+window.minesweeper.checkEMPTY2 = (function(localArr){
+length = localArr.length 
+console.log(localArr);
+  if ( localArr[0] > 0 ) {
+    if ( Number.isInteger(gridMines[(localArr[0])-1][localArr[1]]) && !tempArray.join('').includes(''+(localArr[0]-1)+''+localArr[1]+'') ) {
+      if (gridMines[(localArr[0])-1][localArr[1]] == 0 ) {
+        tempArray.push(localArr[0]-1,localArr[1], "x");
+        tempArray2.push(localArr[0]-1,localArr[1]);
+        localArr.push(localArr[0]-1,localArr[1]);
+      } else {
+        tempArray.push(localArr[0]-1,localArr[1], "x");
+        tempArray2.push(localArr[0]-1,localArr[1]);
+      };
+    };
+  }; 
+
+  if (  localArr[0] < gridHeight-1 ) {
+    if ( Number.isInteger(gridMines[(localArr[0])+1][localArr[1]]) && !tempArray.join('').includes(''+(localArr[0]+1)+''+localArr[1]+'') )  {
+      if (gridMines[(localArr[0])+1][localArr[1]] == 0 ) {
+        tempArray.push(localArr[0]+1,localArr[1], "x");
+        tempArray2.push(localArr[0]+1,localArr[1]);
+        localArr.push(localArr[0]+1,localArr[1]);
+      } else {
+        tempArray.push(localArr[0]+1,localArr[1], "x");
+        tempArray2.push(localArr[0]+1,localArr[1]);
+      };
+    };
+  }; 
+
+  if ( localArr[1] > 0 ) {
+    if ( Number.isInteger(gridMines[localArr[0]][(localArr[1])-1]) &&  !tempArray.join('').includes(''+(localArr[0])+''+(localArr[1]-1)+'') )  {
+      if (gridMines[(localArr[0])][localArr[1]-1] == 0 ) {
+        tempArray.push(localArr[0],localArr[1]-1, "x");
+        tempArray2.push(localArr[0],localArr[1]-1);
+        localArr.push(localArr[0],localArr[1]-1);
+      } else {
+        tempArray.push(localArr[0],localArr[1]-1, "x");
+        tempArray2.push(localArr[0],localArr[1]-1);
+      };
+    };
+  };
+
+  if ( localArr[1] < gridHeight-1 ) { 
+    if ( Number.isInteger(gridMines[localArr[0]][(localArr[1])+1]) &&  !tempArray.join('').includes(''+(localArr[0])+''+(localArr[1]+1)+'') ) {
+      if (gridMines[(localArr[0])][localArr[1]+1] == 0 ) {
+        tempArray.push(localArr[0],localArr[1]+1, "x");
+        tempArray2.push(localArr[0],localArr[1]+1);
+        localArr.push(localArr[0],localArr[1]+1);
+      } else {
+        tempArray.push(localArr[0],localArr[1]+1, "x");
+        tempArray2.push(localArr[0],localArr[1]+1);
+      };
+    };
+  }; 
+
+  localArr.splice(0,2);
+  if (localArr.length > 0 ) {
+    window.minesweeper.checkEMPTY2(localArr);  
+  };
+});
+ /*
 window.minesweeper.checkemptyup = (function(array) {
 
   localArr = array.slice(0);
 
   for ( var i = 0; i < localArr.length; i += 2 ) {
     if ((localArr[i]) > 0) { 
-      // up
       if ( gridMines[(localArr[i])-1][localArr[i+1]] == 0 ) {
         if ( tempArray.join('x').includes(''+(localArr[i])-1+','+localArr[i+1]+'') ) {
           console.log('broked');
@@ -226,7 +397,7 @@ window.minesweeper.checkemptydown = (function(array) {
   localArr = array.slice(0);
 
   for ( var i = 0; i < localArr.length; i += 2 ) {
-    if ((localArr[i]) < 9) { 
+    if ((localArr[i]) < gridHeight - 1) { 
       if ( gridMines[(localArr[i])+1][localArr[i+1]] == 0 ) {
         if ( tempArray.join('x').includes(''+(localArr[i])+1+','+localArr[i+1]+'') ) {
           console.log('broked');
@@ -297,7 +468,7 @@ window.minesweeper.checkemptyright = (function(array) {
   localArr = array.slice(0);
   console.log('right', tempArray, localArr);
   for ( var i = 0; i < localArr.length; i += 2 ) {
-    if ((localArr[i+1]) < 9) { 
+    if ((localArr[i+1]) < gridHeight - 1) { 
       if ( gridMines[localArr[i]][(localArr[i+1])+1] == 0 ) {
         if ( tempArray.join('x').includes(''+localArr[i]+','+localArr[i+1]+1+'') ) {
           console.log('broked');
@@ -328,3 +499,32 @@ window.minesweeper.checkemptyright = (function(array) {
     window.minesweeper.checkemptyright(localArr);
   };
 });
+*/
+
+window.minesweeper.gameover = function(status) {
+  $('#fail').toggle();
+  if ( status == "fail" ) {
+    document.getElementById('smile').innerHTML = '<p>&#9760;</p>';
+  } else {
+    document.getElementById('fail').innerHTML = '<p>You won!</p>';
+    document.getElementById('smile').innerHTML = '<p>&#9996;</p>';
+  };
+};
+
+window.minesweeper.checkWin = function() {
+checkedFields = 0;
+
+  for ( var i = 0; i < gridHeight; i++) {
+    for ( var j = 0; j < gridWidth; j++) {
+      if ( gridMines[i][j] != "m" && $('#'+i+'x'+j+'').attr('class').includes('checked') ) {
+        checkedFields++;         
+      };
+    };
+  };
+
+  if ( checkedFields == ( gridHeight * gridHeight)  - minesNumber ) {
+    window.minesweeper.gameover("win"); 
+  };
+};
+
+    
